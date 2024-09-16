8610872652eb1ccd20a6f9595a642e921424af77
Chaos-19 <kalgetachew375@gmail.com>
2024-09-16 17:51:07 +0300

    add git workflow

--- src/components/Jsoncrack.jsx
+++ src/components/Jsoncrack.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect ,useRef} from "react";
 import { JSONGraph as Graph } from "jsongraph-react";
 import { fileExtensions } from "../constants";
 const fs = acode.require("fsOperation");
